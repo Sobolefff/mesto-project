57cdf31dc91f5236b030b38cbf65becf21b8f8a2
Sobolefff <89976955+Sobolefff@users.noreply.github.com>
2022-06-02 14:45:07 +0300

    правки ревью

--- src/components/index.js
+++ src/components/index.js
@@ -41,7 +41,7 @@ import { getUserInfo, getCards } from './api.js';
   
 // перебираем каждый попап
 allPopups.forEach((popup) => {
-  popup.addEventListener('mouseup', (evt) => {
+  popup.addEventListener('mousedown', (evt) => {
       if (evt.target.classList.contains('popup__close-button') || evt.target.classList.contains('popup_opened')) {
         closePopup(popup)
       }
@@ -93,8 +93,7 @@ const disabledButton = button => {
   formElementAddCard.addEventListener('submit', (evt) => {
     evt.preventDefault();
     handlePlaceFormSubmit(evt, formElementAddCard, linkInput.value, titleInput.value);
-  }); 
-// Геннадий, спасибо за оперативное ревью! По поводу бага с событием клика вы наверное хотели сказать не mousedown а mouseup))
+  });
   
 
   Promise.all([getUserInfo(), getCards()])
